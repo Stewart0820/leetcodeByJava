e8c1df1c4305cd6c35d91ca4c86be64d3ca7df88
Stewart <sukura13123133927@qq.com>
2021-10-19 21:53:49 +0800

    2021 10 19晚上

---
+++ src/com/stewart/algorithms/初级算法20211016/数组/Solution10.java
@@ -0,0 +1,57 @@
+package com.stewart.algorithms.初级算法20211016.数组;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Stewart
+ * @create 2021/10/18
+ */
+public class Solution10 {
+    private static boolean isValidSudoku(String[][] board) {
+
+        for(int i = 0;i < 9;i++){
+            HashSet setLine = new HashSet();
+            HashSet setCol = new HashSet();
+            HashSet setBox = new HashSet();
+            for (int j = 0; j < 9; j++) {
+                if(board[i][j] != "." &&!setLine.add(board[i][j])){
+                    return false;
+                }
+                if(board[j][i] != "." && !setCol.add(board[j][i])){
+                    return false;
+                }
+                int a = (i/3)*3 + j/3;
+                int b = (i%3)*3 + j%3;
+                if(board[a][b] != "." && !setBox.add(board[a][b])){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+
+
+    }
+
+    //    private static boolean isValid(Set set,String ch){
+//
+//
+//        return false;
+//    }
+    public static void main(String[] args) {
+        String[][] arr = {{"5", "3", ".", ".", "7", ".", ".", ".", "."}
+                , {"6", ".", ".", "1", "9", "5", ".", ".", "."}
+                , {".", "9", "8", ".", ".", ".", ".", "6", "."}
+                , {"8", ".", ".", ".", "6", ".", ".", ".", "3"}
+                , {"4", ".", ".", "8", ".", "3", ".", ".", "1"}
+                , {"7", ".", ".", ".", "2", ".", ".", ".", "6"}
+                , {".", "6", ".", ".", ".", ".", "2", "8", "."}
+                , {".", ".", ".", "4", "1", "9", ".", ".", "5"}
+                , {".", ".", ".", ".", "8", ".", ".", "7", "9"}};
+
+        isValidSudoku(arr);
+    }
+
+}
---
+++ src/com/stewart/algorithms/初级算法20211016/数组/Solution11.java
@@ -0,0 +1,28 @@
+package com.stewart.algorithms.初级算法20211016.数组;
+
+/**
+ * @author Stewart
+ * @create 2021/10/18
+ */
+public class Solution11 {
+    public static void rotate(int[][] matrix) {
+        int[][] ints = new int[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                ints[j][ints.length-i-1]= matrix[i][j];
+            }
+        }
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints.length; j++) {
+                matrix[i][j] = ints[i][j];
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3},
+                        {4,5,6},
+                        {7,8,9}};
+        rotate(arr);
+    }
+}
